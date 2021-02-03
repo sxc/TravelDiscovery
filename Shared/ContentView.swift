86eeb13ef3e3dc02dfc0c1f5d8f514e7469fca36
sxc <qiuyequ@icloud.com>
2021-02-03 14:10:37 +0800

    add four view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -14,10 +14,106 @@ struct ContentView: View {
                 
                 DiscoverCategoriesView()
                 
+                PopularDestinationsView()
+                
+                PopularRestaurantsView()
+                
+                TrendingCreatorsView()
+                
             }
                 .navigationTitle("Discover")
         }
     }
+}
+
+struct PopularDestinationsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular destinations")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                
+                HStack(spacing: 8.0) {
+                    ForEach(0..<5, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 125, height: 150)
+                            .background(Color.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
+
+
+struct PopularRestaurantsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular places to eat")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                
+                HStack(spacing: 8.0) {
+                    ForEach(0..<5, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 200, height: 64)
+                            .background(Color.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
+
+
+struct TrendingCreatorsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Trending Creators")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+            .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8.0) {
+                    ForEach(0..<15, id: \.self) { num in
+                        VStack {
+                            Spacer()
+                                .frame(width: 50, height: 50)
+                                .background(Color.gray)
+                                .cornerRadius(.infinity)
+                                .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                                .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
