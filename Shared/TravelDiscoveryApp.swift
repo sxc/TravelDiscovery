16486f0a53160cfe0cd991eadd5af927aede0099
sxc <qiuyequ@icloud.com>
2021-02-03 09:32:00 +0800

    Initial Commit

---
+++ Shared/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  Shared
+//
+//  Created by Xiaochun Shen on 2021/2/3.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
