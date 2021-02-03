1b06ad9e92ddbc5fd631be0816e3e5cd9171691a
sxc <qiuyequ@icloud.com>
2021-02-03 13:09:02 +0800

    add discover categories view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                
+                DiscoverCategoriesView()
+                
+            }
+                .navigationTitle("Discover")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiscoverCategoriesView: View {
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 8) {
+                ForEach(0..<7, id: \.self) { num in
+                    
+                    VStack {
+                        Spacer()
+                            .frame(width: 60, height: 60)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                        Text("Art")
+                            .font(.system(size: 12, weight: .semibold))
+                    }
+                    
+                }
+                
+            }.padding(.horizontal)
+            
+        }
     }
 }
