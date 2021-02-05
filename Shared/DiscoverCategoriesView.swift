a991889da114973bad7c99b1762c9fc23be0e102
sxc <qiuyequ@icloud.com>
2021-02-05 15:10:39 +0800

    fetch JSON data

--- Shared/DiscoverCategoriesView.swift
+++ Shared/DiscoverCategoriesView.swift
@@ -48,19 +48,51 @@ struct DiscoverCategoriesView: View {
     }
 }
 
+
+struct Place: Decodable, Hashable {
+    let  name, thumbnail: String
+}
+
 class CategoryDetailsViewModel: ObservableObject {
     
-    @Published var isLoading = false
-    @Published var places = [Int]()
+    @Published var isLoading = true
+    @Published var places = [Place]()
+    @Published var errorMessage = ""
     
     init() {
         // network code will happen here
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.isLoading = true
-            self.places = [1,2,3,4,5,6,7]
+        
+        // real network cod
+        
+        guard let url = URL(string:
+                                "https://travel.letsbuildthatapp.com/travel_discovery/category?name=art") else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            
+            // you want to check resp statusCodde and err
+            
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                
+                guard let data = data else { return }
+                do {
+                    
+                    self.places = try JSONDecoder().decode([Place].self, from: data)
+                } catch {
+                    print("Failed to decode JSON:", error)
+                    self.errorMessage = error.localizedDescription
+                    
+                }
+                
+                self.isLoading = false
+//                self.places = [1]
+            }
+            
+        }.resume()
+        
         }
     }
-}
+
 
 struct ActivityIndicatorView: UIViewRepresentable {
     func makeUIView(context: Context) -> UIActivityIndicatorView {
@@ -99,20 +131,25 @@ struct CategoryDetialsView: View {
                 
                 
             } else {
-                ScrollView {
-                    ForEach(vm.places, id: \.self) { num in
-                        VStack(alignment: .leading, spacing: 0) {
-                            Image("art1")
-                                .resizable()
-                                .scaledToFill()
-                            Text("Demo")
-                                .font(.system(size: 12, weight: .semibold))
-                                .padding()
-                                
-                        }.asTile()
-                        .padding()
+                
+                
+                ZStack {
+                    Text(vm.errorMessage)
+                    ScrollView {
+                        ForEach(vm.places, id: \.self) { place in
+                            VStack(alignment: .leading, spacing: 0) {
+                                Image("art1")
+                                    .resizable()
+                                    .scaledToFill()
+                                Text(place.name)
+                                    .font(.system(size: 12, weight: .semibold))
+                                    .padding()
+                                    
+                            }.asTile()
+                            .padding()
+                        }
                     }
-                    }
+                }
                 }
                 
         }
