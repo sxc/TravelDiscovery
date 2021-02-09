c14c1d837651f90dd224b46a0a27f7122e18ae33
sxc <qiuyequ@icloud.com>
2021-02-09 13:59:23 +0800

    custom the review

--- Shared/Restaurant/RestaurantDetailsView.swift
+++ Shared/Restaurant/RestaurantDetailsView.swift
@@ -11,6 +11,19 @@ import Kingfisher
 struct RestaurantDetails: Decodable {
     let description: String
     let popularDishes: [Dish]
+    let photos: [String]
+    let reviews: [Review]
+}
+
+struct Review: Decodable, Hashable {
+    let user: ReviewUser
+    let rating: Int
+    let text: String
+}
+
+struct ReviewUser: Decodable, Hashable {
+    let id: Int
+    let username, firstName, lastName, profileImage: String
 }
 
 struct Dish: Decodable, Hashable {
@@ -107,20 +120,26 @@ struct RestaurantDetailsView: View {
                     .font(.system(size: 14, weight: .regular))
             }.padding()
             
-            HStack {
-                Text("Popular Disher")
-                    .font(.system(size: 16, weight: .bold))
-                Spacer()
-                
-            } .padding(.horizontal)
+//            HStack {
+//                Text("Popular Disher")
+//                    .font(.system(size: 16, weight: .bold))
+//                Spacer()
+//
+//            } .padding(.horizontal)
+//
+//            ScrollView(.horizontal, showsIndicators: false) {
+//                HStack(spacing: 16) {
+//                    ForEach(vm.details?.popularDishes ?? [], id: \.self) { dish in
+//                        DishCell(dish: dish)
+//                    }
+//                }.padding(.horizontal)
+//            }
             
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 16) {
-                    ForEach(vm.details?.popularDishes ?? [], id: \.self) { dish in
-                        DishCell(dish: dish)
-                    }
-                }.padding(.horizontal)
+            if let reviews = vm.details?.reviews {
+                ReviewsList(reviews: reviews)
             }
+          
+            
         }
         .navigationBarTitle("Restaurant Details", displayMode: .inline)
     }
@@ -131,12 +150,73 @@ struct RestaurantDetailsView: View {
     ]
 }
 
+
+struct ReviewsList: View {
+    
+    let reviews: [Review]
+    
+    
+    var body: some View {
+        HStack {
+            Text("Customer Reviews")
+                .font(.system(size: 16, weight: .bold))
+            Spacer()
+            
+        } .padding(.horizontal)
+        
+//        if let reviews = reviews {
+            ForEach(reviews, id: \.self) { review in
+                VStack(alignment: .leading) {
+                    HStack {
+                        KFImage(URL(string: review.user.profileImage))
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 44)
+                            .clipShape(Circle())
+                            
+                        VStack {
+                            Text("\(review.user.firstName) \(review.user.lastName)")
+                                .font(.system(size: 14, weight: .bold))
+                            
+                            HStack(spacing: 4) {
+                                ForEach(0..<review.rating, id: \.self) { num in
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(.orange)
+                                        
+                                }
+                                
+                                ForEach(0..<5 - review.rating, id: \.self) { num in
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(.gray)
+                                        
+                                }
+                            }
+                            
+                        }
+                        
+                        
+                        
+                        Spacer()
+                        Text("Dec 2020")
+                            .font(.system(size: 14, weight: .bold))
+                        
+                    }
+                            Text(review.text)
+                                .font(.system(size: 14, weight: .regular))
+                }.padding(.top)
+                .padding(.horizontal)
+            }
+//        }
+        
+        
+    }
+}
+
 struct DishCell: View {
     let dish: Dish
     
     var body: some View {
         
-       
                 ZStack(alignment: .bottomLeading) {
                     KFImage(URL(string: dish.photo))
                         .resizable()
